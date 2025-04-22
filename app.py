dc0f5c81c11152b3f7162e5e7c8b96e6280e0c02
limjoobin <joobinlim@outlook.com>
2025-04-22 18:40:21 +0800

    Initial commit for frontend

---
+++ app.py
@@ -0,0 +1,27 @@
+"""
+    Frontend interface for askjaime platform
+    Check this out: https://www.gradio.app/guides/agents-and-tool-usage
+"""
+import gradio as gr
+
+# Put it all here for now, eventually will move it into their own individual file for easier customization.
+example_messages = [
+    {"text": "Give me an example of ..."}
+]
+
+chatbot = gr.Chatbot(placeholder="I am Jaime ask me anything!",
+                    type="messages")
+
+with gr.Blocks() as demo:
+    chat_interface= gr.ChatInterface(
+        fn=lambda message, history: "dog",
+        type="messages",
+        title="Ask Jaime",
+        description="placeholder description",
+        examples=example_messages,
+        chatbot=chatbot
+    )
+
+if __name__ == '__main__':
+    # Use environment variable GRADIO_SERVER_NAME and GRADIO_SERVER_PORT to set host and port
+    demo.launch(share=False)
