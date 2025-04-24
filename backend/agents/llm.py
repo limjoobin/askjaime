a829a4c7d96d05c4663eaad21c8d1bf59fbe4b90
Siang Hwee <siang_hwee@hotmail.com>
2025-04-24 11:13:54 +0800

    Create the base llm object to connect with the local LLM server

---
+++ backend/agents/llm.py
@@ -0,0 +1,11 @@
+"""
+This file is used to define the LLM agent to connect to the local LLM server.
+It should be used in all the agents to refer to the llm
+"""
+from langchain_openai import OpenAI
+
+llm = OpenAI(base_url="http://localhost:8080/v1",
+             api_key="not_needed")
+
+if __name__ == "__main__":
+    print(llm.invoke("What is the capital of France?"))
