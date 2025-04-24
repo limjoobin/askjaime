c3f7b2a3ad77d1c42ef519dbcbd04873b7ece510
Siang Hwee <siang_hwee@hotmail.com>
2025-04-24 16:04:25 +0800

    Update to use the newer API

--- backend/agents/llm.py
+++ backend/agents/llm.py
@@ -2,10 +2,13 @@
 This file is used to define the LLM agent to connect to the local LLM server.
 It should be used in all the agents to refer to the llm
 """
-from langchain_openai import OpenAI
+from langchain_openai import ChatOpenAI
 
-llm = OpenAI(base_url="http://localhost:8080/v1",
-             api_key="not_needed")
+llm = ChatOpenAI(
+        base_url="http://localhost:8080/v1",
+        api_key="not_needed",
+        model="qwen2.5-7b"
+    )
 
 if __name__ == "__main__":
     print(llm.invoke("The meaning of life is"))
